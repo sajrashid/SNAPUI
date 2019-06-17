b92c0d8f81a3b9516c574323a6bc0e50b13c9036
sajrashid <sajid.rashid@live.co.uk>
2019-06-17 21:50:45 +0100

    added dynamic table deleted node cache / bin modules

--- src/pages/tests.js
+++ src/pages/tests.js
@@ -1,14 +1,39 @@
 ﻿import React from 'react'
 import { Tab} from 'semantic-ui-react'
 import DataTable from '../component/Table'
+import DTable from '../component/D-Table'
+
+
+
+
+
+
+
+export default class Tests extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        tData:[
+          {'fruit': 'Apple', 'cost': 100},
+          {'fruit': 'Orange', 'cost': 50},
+          {'fruit': 'Banana', 'cost': 35},
+          {'fruit': 'Mango', 'cost': 70},
+          {'fruit': 'Pineapple', 'cost': 45},
+          {'fruit': 'Papaya', 'cost': 40},
+          {'fruit': 'Watermelon', 'cost': 35}
+        ]
+    }
+
+    
+}
+render() {
+    
 const json = [
     { name: 'John', age: 15, gender: 'Male' },
     { name: 'Amber', age: 40, gender: 'Female' },
     { name: 'Leslie', age: 25, gender: 'Female' },
     { name: 'Ben', age: 70, gender: 'Male' },
 ]
-
-
 
 const mainPanes = [
     {
@@ -30,38 +55,40 @@ const mainPanes = [
              </Tab.Pane>
     }
 ]
-const childPanes = [
-    {
-        menuItem: 'SNow API', render: () =>
-            <Tab.Pane attached={false}>
-            Mule-Soft API Tests
-            <DataTable tableData={json} color='red'inverted={false} />
-            </Tab.Pane>
-    },
-    {
-        menuItem: 'GSD API', render: () =>
-            <Tab.Pane attached={false}>
-             GSD API Tests
-              </Tab.Pane>
-    },
-    {
-        menuItem: 'Domino API', render: () =>
-            <Tab.Pane attached={false}>
-             Domino API Tests
-              </Tab.Pane>
-    }
-]
+    const childPanes = [
+        {
+            menuItem: 'SNow API', render: () =>
+                <Tab.Pane attached={false}>
+                Mule-Soft API Tests
+                <DataTable tableData={json} color='red'inverted={false} />
+                <DTable  data={this.state.tData}/>
 
-
-
-const Tests = () => (
-
-    <div>
+      
+               
+                </Tab.Pane>
+        },
+        {
+            menuItem: 'GSD API', render: () =>
+                <Tab.Pane attached={false}>
+                 GSD API Tests
+                  </Tab.Pane>
+        },
+        {
+            menuItem: 'Domino API', render: () =>
+                <Tab.Pane attached={false}>
+                 Domino API Tests
+                  </Tab.Pane>
+        }
+    ]
+    return (
+        <div>
         <Tab menu={{ pointing: true , color: "grey",  inverted: true }} panes={mainPanes} />
            
         <h1>Welcome to the Tests page!</h1>
-    </div>
-)
-
-
-export default Tests
+        </div>
+                
+        );
+    }
+    
+   
+}
